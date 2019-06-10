31bb3482f3567b00735eddca6232c851f9ad254a
yicheny <1463020554@qq.com>
2019-06-10 11:14:31 +0800

    面包屑组件样式修改

--- src/component/Breadcrumb_w.js
+++ src/component/Breadcrumb_w.js
@@ -3,14 +3,15 @@ import {Link, Route} from 'react-router-dom';
 import './Breadcrumb_w.less';
 
 const Breadcrumb_w = ({match, ...rest,}) => {
-    const {name,separator} = rest;
+    const {name, separator} = rest;
     return (
         <span>
-      <Link to={match.url || ''} className={match.isExact ? 'breadcrumb current' : 'breadcrumb'}>
+      <Link to={match.url || ''} className={['x_breadcrumb', match.isExact ? 'current' : ''].join(' ')}>
           {name.shift()}
           {!match.isExact && <span className="separator"> {separator} </span>}
       </Link>
-      <Route path={`${match.url}/:path`} render={(props) => <Breadcrumb_w {...props} name={name} separator={separator}/>}/>
+      <Route path={`${match.url}/:path`}
+             render={(props) => <Breadcrumb_w {...props} name={name} separator={separator}/>}/>
     </span>
     )
 }
