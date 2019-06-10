9419040988c87e8d8d08cd74a7de705a08c6d81f
yicheny <1463020554@qq.com>
2019-06-10 14:20:29 +0800

    login页跳转调整

--- src/login.js
+++ src/login.js
@@ -2,6 +2,6 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 function Login(){
-    return <Link to='/demo'>登录</Link>
+    return <Link to='/cDemo'>登录</Link>
 }
 export default Login;
