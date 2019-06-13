e7f0a9faf5434a5a220948f00f0d8ae4bbe2eace
YLF <1463020554@qq.com>
2019-06-13 20:37:57 +0800

    路由传参各种方式测试

--- src/view/business/Demo4.js
+++ src/view/business/Demo4.js
@@ -3,7 +3,7 @@ import {Button} from 'antd'
 
 function Demo4(props) {
     const handleClick = ()=>{
-        props.history.push({ pathname : '/wDemo/demo3' ,query : { param:[1,2,3,4,5,6]} })
+        props.history.push({ pathname : '/wDemo/demo3' , search:'?id=100&&name=ylf',query : { param:[1,2,3,4,5,6]} })
     };
     return <div>
         <Button onClick={handleClick} type={"primary"}>点击跳转到Demo3</Button>
@@ -11,3 +11,8 @@ function Demo4(props) {
 }
 
 export default Demo4;
+
+// 关于路由传参，网上所说的三种方式params、query、state，其中，query、state自react-router4.x更新后就无法持久保存传参
+// 目前，我认为可以通过search和params进行传参，其中通过search传参不会影响到路由判定，而params的优势在于无论是前进还是后退都可以拿到传参【包括刷新】，search仅可以在前进和刷新时拿到传参【注意：如果想要使用params传参，则必须使用动态路由】
+// 注：hash是用于页面锚点的，但是它的特性和search参数非常相似，也可以用于传参，但是不推荐
+// state可以用于传临时的大量数据
