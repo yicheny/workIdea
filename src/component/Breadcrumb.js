d6ef0078ae0e87a197e8cb21adef75724700d471
YLF <1463020554@qq.com>
2019-06-19 23:09:31 +0800

    面包屑组件实现改进

--- src/component/Breadcrumb.js
+++ src/component/Breadcrumb.js
@@ -1,22 +1,19 @@
-import React,{Fragment} from 'react';
+import React from 'react';
 import './Breadcrumb.less';
-import {Link} from "react-router-dom";
-import {RenderRoute} from "../view/router/Wrapper_Router";
+import {Link,Route} from "react-router-dom";
 
-const BreadcrumbItem = ({match,match:{path},Name,separator='/',...rest}) =>{
-    return <Fragment>
-        <Link to={path}>
+const BreadcrumbItem = ({match,match:{path},Name,separator='/'}) =>{
+    return <Link to={path}>
             <span className={match.isExact?'current':''}>{Name[path]}</span>
             {!match.isExact && <span className="separator"> {separator} </span>}
         </Link>
-    </Fragment>
 };
 
 const Breadcrumb = ({Name}) => {
     return <div className='x_breadcrumb'>
         {
             Name && Object.keys(Name).map((el,i)=>{
-                return <RenderRoute key={i} path={el} component={BreadcrumbItem} Name={Name}/>
+                return <Route key={i} path={el} render={(props)=><BreadcrumbItem {...props} Name={Name}/>}/>
             })
         }
     </div>
--- src/view/router/Wrapper_Router.js
+++ src/view/router/Wrapper_Router.js
@@ -4,6 +4,5 @@ import {Route} from "react-router-dom";
 export const RenderRoute = ({ component: Component, path, ...rest }) => {
     return <Route path={path} render={routeProps => {
         return <Component {...routeProps} {...rest}/>
-    }
-    }/>
+    }}/>
 };
