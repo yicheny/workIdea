65e38b4839921ad0ea495ab5efa3777385293e7c
YLF <1463020554@qq.com>
2019-06-23 13:14:21 +0800

    背景动画效果完成

--- src/component/BG/BG.js
+++ src/component/BG/BG.js
@@ -5,15 +5,18 @@ import Pic02 from "../../asset/img/BG02.jpg"
 import Pic03 from "../../asset/img/BG03.jpg"
 import Pic04 from "../../asset/img/BG04.png"
 import Pic05 from "../../asset/img/BG05.png"
+import Pic06 from "../../asset/img/BG06.jpg"
 
 function Bg(props) {
+    const picArr = [Pic01,Pic02,Pic03,Pic04,Pic05,Pic06];
+
     return (
         <div className='x_bg'>
-            <img className='pic1' src={Pic01} alt=""/>
-            <img className='pic2' src={Pic02} alt=""/>
-            <img className='pic3' src={Pic03} alt=""/>
-            <img className='pic4' src={Pic04} alt=""/>
-            <img className='pic5' src={Pic05} alt=""/>
+            {
+                picArr.map((el,i,thisArg)=> {
+                    return <span style={{backgroundImage:`url(${el})`,animationDelay:`${6*i}s`}}></span>
+                })
+            }
         </div>
     );
 }
