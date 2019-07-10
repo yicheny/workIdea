b6bcea0ea253f8a84e3addd45de4be423e8a3ed7
yicheny <1463020554@qq.com>
2019-07-10 14:43:31 +0800

    获取服务端数据

--- src/view/httpTest/HttpHome.js
+++ src/view/httpTest/HttpHome.js
@@ -1,10 +1,17 @@
-import React, {useEffect} from 'react';
+import React, {useState,useEffect} from 'react';
 import axios from 'axios'
 
 function HttpHome(props) {
+    const [info,setInfo] = useState('')
+
+    useEffect(()=>{
+        axios.get('http://localhost:5000/login').then(res=>{
+            setInfo(res.data)
+        })
+    });
     return (
         <div>
-
+            {info}
         </div>
     );
 }
