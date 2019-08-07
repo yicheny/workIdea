0f17941258f54b4193d232ed7ab116ab3240bcac
YLF <1463020554@qq.com>
2019-08-07 23:40:44 +0800

    计算器样式调整

--- src/view/designPattern/control/Order2.js
+++ src/view/designPattern/control/Order2.js
@@ -1,14 +1,11 @@
-import React,{useState} from 'react';
+import React, {useRef, useState} from 'react';
 import {Button} from "../../../component";
 import './Order2.less';
-import _ from 'lodash'
 
-const btns = ['C','DEL','CANCEL','+','-','*','/',1,2,3,4,5,6,7,8,9,0,'=','RE'];
-
+const btns = ['C','DEL','+','-','*','/',1,2,3,4,5,6,7,8,9,0,'='];
 const isFuncBtns = function (value) {
     return ['C','DEL','RE','=','CANCEL'].includes(value)
 };
-
 const isOperation = function(value){
     return ['+','-','*','/'].includes(value);
 };
@@ -17,6 +14,7 @@ function Order2(props) {
     const [temp,setTemp] = useState('');
     const [opera,setOpera] = useState('');
     const [curr,setCurr] = useState(0);
+    // const order = useRef(new OrderParse(btnClick));
 
     //算法
     const scheduler = {
@@ -64,16 +62,17 @@ function Order2(props) {
         }
         setCurr(Number(curr+value))
     }
+
     return (
         <div className='x_order2' style={{margin:20}}>
             <h3>命令模式-实现回放</h3>
             <h4>1. =：进行运算，显示结果【连续点击，可连续运算】</h4>
             <h4>2. C：清除运算</h4>
             <h4>3. DEL：删除一位</h4>
-            <h4>4. CANCEL：撤销到上一次运算结果【最多支持100次撤销】</h4>
-            <h4>5. RE：回放操作</h4>
+            {/*<h4>4. CANCEL：撤销到上一次运算结果【最多支持100次撤销】</h4>*/}
+            {/*<h4>5. RE：回放操作</h4>*/}
 
-            <div className="calputer box" style={{width:280,paddingTop:0}}>
+            <div className="calputer box" style={{width:280,paddingTop:0,background:'#fff'}}>
                 <div className="calputer_show box">
                     <div className="calputer_course">
                         {temp + opera}
@@ -83,9 +82,11 @@ function Order2(props) {
                     </div>
                 </div>
                 <div className="calputer_main">
+                    <Button>CANCEL</Button>
                     {
                         btns.map((btn,i)=><Button key={i} onClick={(e)=>btnClick(e.target.innerHTML)}>{btn}</Button>)
                     }
+                    <Button>RE</Button>
                 </div>
             </div>
         </div>
