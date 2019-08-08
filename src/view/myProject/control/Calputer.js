948abfbcd11a7d8ad0fb5b7e1481d732d307a1a5
yicheny <1463020554@qq.com>
2019-08-08 10:38:50 +0800

    计算器添加clear功能

--- src/view/myProject/control/Calputer.js
+++ src/view/myProject/control/Calputer.js
@@ -1,16 +1,15 @@
-import React, {useRef, useState} from 'react';
+import React, {useState} from 'react';
 import {Button} from "../../../component";
 import './Calputer.less';
 
-const btns = ['C','DEL','+','-','*','/',1,2,3,4,5,6,7,8,9,0,'='];
-const isFuncBtns = value=>['C','DEL','RE','=','CANCEL'].includes(value);
+const btns = ['CE','DEL','+','-','*','/',1,2,3,4,5,6,7,8,9,0,'=','CLEAR'];
+const isFuncBtns = value=>['CE','DEL','=','CLEAR'].includes(value);
 const isOperation = (value)=>['+','-','*','/'].includes(value);
 
 function Calputer(props) {
     const [temp,setTemp] = useState('');
     const [opera,setOpera] = useState('');
     const [curr,setCurr] = useState(0);
-    // const order = useRef(new OrderParse(btnClick));
 
     //算法
     const scheduler = {
@@ -29,18 +28,17 @@ function Calputer(props) {
         '=':()=>{
             scheduler[opera]();
         },
-        C:()=>{
+        CE:()=>{
             setCurr(0)
         },
         DEL:()=>{
             setCurr(Number(curr.toString().substr(0,curr.toString().length-1)))
         },
-        RE:()=>{
-
-        },
-        CANCEL:()=>{
-
-        },
+        CLEAR:()=>{
+            setTemp('');
+            setOpera('');
+            setCurr(0);
+        }
     };
 
     function btnClick(value) {
@@ -60,10 +58,9 @@ function Calputer(props) {
         <div className='x_order2' style={{margin:20}}>
             <h3>命令模式-实现回放</h3>
             <h4>1. =：进行运算，显示结果【连续点击，可连续运算】</h4>
-            <h4>2. C：清除运算</h4>
+            <h4>2. CE：清除运算结果</h4>
             <h4>3. DEL：删除一位</h4>
-            {/*<h4>4. CANCEL：撤销到上一次运算结果【最多支持100次撤销】</h4>*/}
-            {/*<h4>5. RE：回放操作</h4>*/}
+            <h4>4. CLEAR：清空所有</h4>
 
             <div className="calputer box" style={{width:280,paddingTop:0}}>
                 <div className="calputer_show box">
@@ -75,11 +72,9 @@ function Calputer(props) {
                     </div>
                 </div>
                 <div className="calputer_main">
-                    <Button>CANCEL</Button>
                     {
                         btns.map((btn,i)=><Button key={i} onClick={(e)=>btnClick(e.target.innerHTML)}>{btn}</Button>)
                     }
-                    <Button>RE</Button>
                 </div>
             </div>
         </div>
