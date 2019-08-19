3bde2b1f4eb9bd1a42c221f82a323af10ee6a75d
yicheny <1463020554@qq.com>
2019-08-19 18:13:41 +0800

    Tab组件测试数据调整

--- src/component/Tabs/Tabs.js
+++ src/component/Tabs/Tabs.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './Tabs.less';
 
-function Tabs({active,tabData,className,...rest}) {
-    const {onClick} = rest;
+function Tabs(props) {
+    let {active,data,className,onClick} = props;
     const cn = ['x_tabs',className].join(" ");
 
     return (
         <div className={cn}>
             {
-                tabData.map((el,i)=>{
+                data.map((el,i)=>{
                     return<span key={i}
                                 onClick={() => onClick(el.id)}
                                 className={['x_tabs_item',el.id===active?'active':''].join(' ')}>{el.text}</span>
--- src/view/componetentTest/control/TabsTest.js
+++ src/view/componetentTest/control/TabsTest.js
@@ -1,29 +1,51 @@
 import React, {useState} from 'react';
 import {Container, Tabs} from "../../../component";
 
+const mockData = {
+    Tab1:{
+        name:'小明',
+        height:'181',
+        sexy:'男'
+    },
+    Tab2:{
+        name:"小红",
+        height:'158',
+        sexy:'女'
+    },
+    Tab3:{
+        name:'小刚',
+        height:'177',
+        sexy:'男'
+    }
+};
+
 function TabsTest(props) {
     const [active,setActive] = useState('Tab1');
-    const showMap = {
-        Tab1:'Tab1',
-        Tab2:'Tab2',
-        Tab3:'Tab3',
-    }
-    const tabData = [{
-        id:'Tab1',
-        text:"Tab1"
-    }, {
-        id:'Tab2',
-        text:"Tab2"
-    }, {
-        id:'Tab3',
-        text:"Tab3"
-    }];
+    const tabData = [
+        {
+            id:'Tab1',
+            text:'Tab1'
+        },
+        {
+            id:'Tab2',
+            text:'Tab2'
+        },
+        {
+            id:'Tab3',
+            text:'Tab3'
+        }
+    ];
 
+    const data = mockData[active];
     return (
         <div>
             <Container>
-                <Tabs tabData={tabData} active={active} onClick={(v)=>setActive(v)}/>
-                {showMap[active]}
+                <Tabs data={tabData} active={active} onClick={(v)=>setActive(v)}/>
+                <div>
+                    姓名：{data.name}<br/>
+                    身高：{data.height}<br/>
+                    性别：{data.sexy}
+                </div>
             </Container>
         </div>
     );
