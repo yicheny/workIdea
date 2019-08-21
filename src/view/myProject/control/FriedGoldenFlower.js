3a529b956cf82b419a06f8b42d202ca594e2334a
yicheny <1463020554@qq.com>
2019-08-21 13:43:16 +0800

    container组件改进

--- src/view/myProject/control/FriedGoldenFlower.js
+++ src/view/myProject/control/FriedGoldenFlower.js
@@ -4,8 +4,27 @@ import {Container} from "../../../component";
 
 function FriedGoldenFlower(props) {
     return <Container className="x_fried" header='炸金花'>
+        <ul>
+            {creCards().map((el,i)=><li key={i} style={{padding:'6px 0'}}>{el}</li>)}
+        </ul>
+    </Container>;
 
-    </Container>
+    function creCards() {
+        const cards = [];
+        const flowerColors = ['红桃','黑桃','梅花','方块'];
+        const cardValues =  ['3','4','5','6','7','8','9','J','Q','K','A','2'];
+        const kings = ['大王','小王'];
+
+        flowerColors.forEach(el=>{
+            cardValues.forEach(el2=>{
+                cards.push(el + el2);
+            })
+        });
+        cards.concat(kings);
+
+        return cards;
+    }
+
 }
 
 export default FriedGoldenFlower;
