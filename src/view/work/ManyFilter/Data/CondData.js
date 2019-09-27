9a7b7e0055f1346eb3fd56e2435693f83aeb365a
yicheny <1463020554@qq.com>
2019-09-27 10:08:17 +0800

    ManyFilter数据变更

---
+++ src/view/work/ManyFilter/Data/CondData.js
@@ -0,0 +1,27 @@
+export const condData = {
+    condition:[
+        {
+            key:'duration',
+            status:'小于',
+            value:11
+        }
+    ],
+    product:{
+        pools:[],
+        strategy:[],
+    }
+};
+
+/*
+可能存在的status关系
+- 大于
+- 小于
+- 等于
+- 不等于
+- 区间
+- 前
+- 后
+- 前%
+- 后%
+- 区间%
+* */
--- src/view/work/ManyFilter/ManyFilter.js
+++ src/view/work/ManyFilter/ManyFilter.js
@@ -1,34 +1,10 @@
 import React,{Fragment} from "react";
-import {Data} from './Data/data';
+import {condData} from './Data/CondData';
+import {data} from './Data/Data';
 import '../../../asset/style/View/business/demo2.less';
 
 export default class ManyFilter extends React.Component{
-    condSymMap = {
-        '等于': '=',
-        '不等于': '!=',
-        '大于': '>',
-        '小于': '<',
-        '大于等于': '>=',
-        '小于等于': '<=',
-    };
-    condValMap = {
-        '区间':(v)=>`[${v[0]},${v[1]}]`,
-        '区间%':(v)=>`[${v[0]}%,${v[1]}%]`,
-    };
-
     render() {
-        const data = Data().map(item=>Object.assign(item,{show:false}));//通过index可以很容易设置show,显隐控制可以轻松做到
-
-        return <Fragment>
-            {
-                data.map((item,index) =>{
-                    return <div key={index} className={item.show?'work show':"business"}>
-                        <span>{item.text}</span>
-                        <span>{this.condSymMap[item.status] || item.status}</span>
-                        <span>{Array.isArray(item.value)? this.condValMap[item.status](item.value) : item.value}</span>
-                    </div>
-                })
-            }
-        </Fragment>
+        return <div>ManyFilter</div>
     }
 }
