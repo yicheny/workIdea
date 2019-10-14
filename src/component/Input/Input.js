ae03684a85d93397d8f600b61e8d73bfd48a1503
yicheny <1463020554@qq.com>
2019-10-14 15:18:38 +0800

    add 初步实现二进制与十进制相互转换

--- src/component/Input/Input.js
+++ src/component/Input/Input.js
@@ -2,12 +2,12 @@ import React from 'react';
 import './Input.less';
 
 function Input(props) {
-    const {addonBefore,style,className,noBorder,...rest} = props;
+    const {addonBefore,style,className,onChange,onBlur} = props;
     const cn = ["x_input_wrapper",className,addonBefore?'addonBefore':''].join(' ');
 
     return <span className={cn} style={style}>
             {addonBefore && <span className="x_input_addon">{addonBefore}</span>}
-            <input type="text" {...rest}/>
+            <input type="text" onChange={onChange} onBlur={onBlur}/>
         </span>
 }
 
@@ -15,7 +15,7 @@ Input.defaultProps = {
     style:{},
     className:'',
     addonBefore:false,
-    noBorder:false
+    // noBorder:false
 };
 
 export default Input;
--- src/view/myProject/control/systemSwitch/SystemSwitch.js
+++ src/view/myProject/control/systemSwitch/SystemSwitch.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Container,Input} from "../../../../component";
 
 function SystemSwitch(props) {
-    return (
-        <div>进制转换</div>
-    );
+    const [tenNum,setTenNum] = useState(0);
+    const [twoNum,setTwoNum] = useState(0);
+
+    return <Container header='进制转换'>
+        <div>
+            <h3>十进制转二进制:</h3>
+            <Input type='number' value={tenNum} onBlur={(e)=>setTenNum(Number(e.target.value))}/>
+            <span style={{margin:'0 12px'}}>{tenSOther()}</span>
+        </div>
+        <div>
+            <h3>二进制转十进制:</h3>
+            <Input type='number' value={twoNum} onBlur={(e)=>setTwoNum(Number(e.target.value))}/>
+            <span style={{margin:'0 12px'}}>{otherSTen()}</span>
+        </div>
+    </Container>;
+
+    function tenSOther(raidx=2) {
+        return tenNum.toString(raidx);
+    }
+    function otherSTen(radix=2) {
+        return parseInt(twoNum,2)
+    }
 }
 
 export default SystemSwitch;
