9687cf4b4408d4631ff85f51ef78e86508961210
yicheny <1463020554@qq.com>
2019-10-16 10:16:43 +0800

    update 将genPerson函数移回到GenPersonDemo文件中

--- src/view/HugeSuraDemo/genPersonDemo/GenPersonDemo.js
+++ src/view/HugeSuraDemo/genPersonDemo/GenPersonDemo.js
@@ -1,13 +1,18 @@
 import React,{useState} from 'react';
 import {Container,Button} from "../../../component";
-import {GenPerson} from "../utils/GenPerson";
+import {arrCompare, arrRandom, genRandom} from "../../../utils/publicFun";
+import {PersonNameList} from "../baseData/BaseData";
+import IndexedDbClient from "../../../base/IndexedDbClient";
+
+const existsPersonNameList = [];
 
 function GenPersonDemo(props) {
+    const db = new IndexedDbClient('hugeSura',1,'persons');
     const [person,setPerson] = useState({});
 
     return <Container header='人物生成'>
-        <Button onClick={()=>setPerson(GenPerson())}>随机人物</Button>
-        <Button type='primary'>确认生成</Button>
+        <Button onClick={()=>setPerson(randomPerson())}>随机人物</Button>
+        <Button type='primary' onClick={genPerson}>确认生成</Button>
         <div>
             <p>姓名：{person.name}</p>
             <p>性别：{person.sexy}</p>
@@ -20,6 +25,33 @@ function GenPersonDemo(props) {
             <p>自由点数：{person.freePoint}</p>
         </div>
     </Container>;
+
+    function randomPerson(person={}) {
+        const name = person.name || nameFor();
+        const sexy = person.sexy || arrRandom(['man','woman']);
+        const talent = genRandom(1,100);
+
+        return {
+            name,
+            level:1,
+            sexy,
+            hp:genRandom(20,100),
+            attack:genRandom(0,20),
+            defense:genRandom(0,20),
+            speed:genRandom(0,20),
+            talent,
+            freePoint:100-talent,
+        };
+
+        function nameFor() {
+            if (arrCompare(existsPersonNameList,PersonNameList)) return console.error('人物名称已全部使用完成');//临时，不严谨
+            const name = arrRandom(PersonNameList);
+            return existsPersonNameList.includes(name) ? nameFor() : name;
+        }
+    }
+    function genPerson() {
+        db.add(person);
+    }
 }
 
 export default GenPersonDemo;
