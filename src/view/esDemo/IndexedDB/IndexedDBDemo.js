7a44dfc56eb635f7de94e99a4c4d1ff302da8f54
yicheny <1463020554@qq.com>
2019-10-16 09:09:23 +0800

    update 调整默认启动端口为3020;IndexedDBDemo页面允许输入ID进行增删改查

--- src/view/esDemo/IndexedDB/IndexedDBDemo.js
+++ src/view/esDemo/IndexedDB/IndexedDBDemo.js
@@ -1,5 +1,6 @@
 import React,{useState} from 'react';
-import {Container,Button} from "../../../component";
+import {Container, Button, TextInput} from "../../../component";
+import {genRandom} from "../../../utils/publicFun";
 
 class IndexedDbClient{
     constructor(dbName='default',version=1){
@@ -67,15 +68,25 @@ class IndexedDbClient{
     }
 }
 
+const db = new IndexedDbClient();
 function IndexedDbDemo(props) {
-    const [db] = useState(new IndexedDbClient());
+    const [id,setId] = useState(0);
 
     return <Container header='IndexedDbDemo'>
-        <Button type='primary' onClick={()=>db.add({name:'123',attack:1223,begin:12312})}>新增数据</Button>
-        <Button type='primary' onClick={()=>db.edit(2,{name:'la',hp:'ddd'})}>编辑数据</Button>
-        <Button type='primary' onClick={()=>db.query(2)}>查询数据</Button>
-        <Button type='primary' onClick={()=>db.del(3)}>删除数据</Button>
+        <p>请设置Id：<TextInput type='number' onChange={v=>setId(Number(v))}/></p>
+        <Button type='primary' onClick={()=>db.add(genData())}>新增数据</Button>
+        <Button type='primary' onClick={()=>db.edit(id,genData())}>编辑数据</Button>
+        <Button type='primary' onClick={()=>db.query(id)}>查询数据</Button>
+        <Button type='primary' onClick={()=>db.del(id)}>删除数据</Button>
     </Container>;
+    
+    function genData() {
+        return {
+            name:genRandom(0,10000),
+            age:genRandom(10,90),
+            hp:genRandom(0,2000)
+        }
+    }
 }
 
 export default IndexedDbDemo;
