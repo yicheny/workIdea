ab301366594017f65726609cb74c0eb574d78d09
yicheny <1463020554@qq.com>
2019-10-16 11:30:20 +0800

    update TextInput组件检测逻辑调整

--- src/component/Input/Input.js
+++ src/component/Input/Input.js
@@ -2,12 +2,12 @@ import React from 'react';
 import './Input.less';
 
 function Input(props) {
-    const {type,value,addonBefore,style,className,onChange,onBlur,onFocus} = props;
+    const {type,value,addonBefore,style,className,onChange,onBlur,onFocus,...rest} = props;
     const cn = ["x_input_wrapper",className,addonBefore?'addonBefore':''].join(' ');
 
     return <span className={cn} style={style}>
             {addonBefore && <span className="x_input_addon">{addonBefore}</span>}
-            <input value={value} type={type} onChange={onChange} onBlur={onBlur} onFocus={onFocus}/>
+            <input value={value} type={type} onChange={onChange} onBlur={onBlur} onFocus={onFocus} {...rest}/>
         </span>
 }
 
--- src/component/TextInput/TextInput.js
+++ src/component/TextInput/TextInput.js
@@ -3,11 +3,11 @@ import {Input} from "../index";
 import {last,isNumber} from "../../utils/publicFun";
 
 function TextInput(props) {
-    const {system,type,onChange} = props;
+    const {system,type,onChange,placeholder} = props;
     const [value,setValue] = useState(props.value);
 
     return <span className="textInput">
-        <Input value={value} onFocus={handleFocus} onBlur={()=>onChange(value)} onChange={e=>handleChange(e.target.value)}/>
+        <Input value={value} onFocus={handleFocus} onBlur={()=>onChange(value)} onChange={e=>handleChange(e.target.value)} placeholder={placeholder}/>
     </span>;
 
     function handleFocus() {
@@ -16,22 +16,25 @@ function TextInput(props) {
 
     function handleChange(v) {
         if(v==='') return setValue('');
-        if(!systemCheck()) return;
         if(!typeCheck()) return;
         return setValue(v);
 
-        function systemCheck() {
-            function genSystemList() {
-                if(!isNumber(system)||system<2) return console.error('genSystemList函数运行错误',system);
-                const systemList = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'];
-                return Array.from(Array(system),(item,index)=>systemList[index])
-            }
-            return genSystemList().includes(last(v));
-        }
         function typeCheck() {
             const typeStrategy = {
                 text:()=>true,
-                number:()=>!isNaN(Number(v))
+                number:()=>{
+                    if(!systemCheck()) return;
+                    return !isNaN(Number(v));
+
+                    function systemCheck() {
+                        function genSystemList() {
+                            if(!isNumber(system)||system<2) return console.error('genSystemList函数运行错误',system);
+                            const systemList = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'];
+                            return Array.from(Array(system),(item,index)=>systemList[index])
+                        }
+                        return genSystemList().includes(last(v));
+                    }
+                }
             };
             return typeStrategy[type]()
         }
