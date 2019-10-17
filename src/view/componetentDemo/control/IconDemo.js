37e28ec5927c2769abbc5967730e5195abe47791
yicheny <1463020554@qq.com>
2019-10-17 16:56:18 +0800

    add IconDemo页面实现

--- src/view/componetentDemo/control/IconDemo.js
+++ src/view/componetentDemo/control/IconDemo.js
@@ -1,9 +1,30 @@
 import React from 'react';
-import {Container} from "../../../component";
+import {Container, Icon} from "../../../component";
+
+function Item(props) {
+    const {name} = props;
+    return <div className='flex-y center' style={{margin:'6px 12px'}}>
+        <Icon type={name}/>
+        <span>{name}</span>
+    </div>
+}
 
 function IconDemo(props) {
-    return <Container header='IconDemo'>
-
+    return <Container header='IconDemo' >
+        <div className="flex">
+            <Item name='info'/>
+            <Item name='cancel'/>
+            <Item name='arrowDown'/>
+            <Item name='error'/>
+            <Item name='success'/>
+            <Item name='warn'/>
+            <Item name='user'/>
+            <Item name='lock'/>
+            <Item name='unlock'/>
+            <Item name='zhankai'/>
+            <Item name='shousuo'/>
+            <Item name='star'/>
+        </div>
     </Container>
 }
 
