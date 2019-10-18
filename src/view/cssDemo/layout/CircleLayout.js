c3bb1a8d6f2eaead4b0db4b55aa7f9001f02c4b7
yicheny <1463020554@qq.com>
2019-10-18 18:05:30 +0800

    update 环形排列布局进行中...

--- src/view/cssDemo/layout/CircleLayout.js
+++ src/view/cssDemo/layout/CircleLayout.js
@@ -1,10 +1,38 @@
 import React from 'react';
 import {Container} from "../../../component";
+import './CircleLayout.less';
 
 function CircleLayout(props) {
-    return <Container header='环形排列布局'>
+    const boxWidth = 600;
+    const boxHeight = 480;
+    const itemWidth = 60;
+    const itemHeight = 60;
 
-    </Container>
+    main();
+    return <Container header='环形排列布局'>
+        <div className="circle_layout_box flex-y center">
+            <div className="circle_layout_item">1</div>
+            <div className="circle_layout_item">2</div>
+            <div className="circle_layout_item">3</div>
+            <div className="circle_layout_item">4</div>
+            {/*<div className="circle_layout_item">5</div>*/}
+            {/*<div className="circle_layout_item">6</div>*/}
+        </div>
+    </Container>;
+
+    function main() {
+        for(let i=0;i<24;i++){
+            const rad = (((i+3)%4)/2)*Math.PI;
+            const sinValue = Math.sin(rad).toFixed(4);
+            const top = (boxWidth-itemWidth)/2;
+            const left = (boxHeight-itemHeight)/2;
+
+            console.log(30,(sinValue * left)+left);
+            console.log(31,(sinValue+1)*left);
+
+            // console.log((sinValue * top)+top);
+        }
+    }
 }
 
 export default CircleLayout;
