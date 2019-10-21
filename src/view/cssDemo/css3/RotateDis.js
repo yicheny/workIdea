e1dcab2b1362155a9c7917910a66dcb6a5138351
yicheny <1463020554@qq.com>
2019-10-21 10:56:51 +0800

    update 旋转放大动画实现

--- src/view/cssDemo/css3/RotateDis.js
+++ src/view/cssDemo/css3/RotateDis.js
@@ -4,7 +4,11 @@ import './RotateDis.less';
 
 function RotateDis(props) {
     return <Container header='旋转放大显示'>
-
+        <div className="rotate_dis_wrap flex center">
+            <div className="rotate_dis flex center">
+                旋转放大显示
+            </div>
+        </div>
     </Container>
 }
 
