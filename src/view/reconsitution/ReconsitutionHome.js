970044f4b79d256c128752921892fa60994ecf58
yicheny <1463020554@qq.com>
2019-10-23 11:02:22 +0800

    update '重构'页面导航结构调整

--- src/view/reconsitution/ReconsitutionHome.js
+++ src/view/reconsitution/ReconsitutionHome.js
@@ -1,10 +1,19 @@
 import React, {Fragment} from 'react';
 import {Link} from "react-router-dom";
+import {BoxM} from "../../component";
 
 function ReconsitutionHome(props) {
     return <Fragment>
-        <Link to='/recons/class1'>Class1_实例与练习</Link>
-        <Link to='/recons/class2'>Class2_常用重构技巧</Link>
+        <BoxM tit='Class1_实例与练习'>
+            <Link to='/recons/class1/part1'>part1</Link>
+        </BoxM>
+        <BoxM tit='Class2_常用重构技巧'>
+            <Link to='/recons/class2/extractFunction'>提炼函数</Link>
+            <Link to='/recons/class2/inlineFunction'>内联函数</Link>
+            <Link to='/recons/class2/extractVariable'>提炼变量</Link>
+            <Link to='/recons/class2/inlineVariable'>内联变量</Link>
+            <Link to='/recons/class2/changeFunctionDeclaration'>改变函数声明</Link>
+        </BoxM>
     </Fragment>
 }
 
--- src/view/reconsitution/ReconsitutionRouter.js
+++ src/view/reconsitution/ReconsitutionRouter.js
@@ -2,14 +2,24 @@ import React from 'react';
 import './ReconsotutionRouter.less';
 import {Switch, Route} from "react-router-dom";
 import ReconsitutionHome from "./ReconsitutionHome";
-import Class1Router from "./class1/Class1Router";
-import Class2Router from "./class2/Class2Router";
+import Part1 from "./class1/part1";
+import ExtractFunction from "./class2/ExtractFunction";
+import InlineFunction from "./class2/InlineFunction";
+import ExtractVariable from "./class2/ExtractVariable";
+import InlineVariable from "./class2/InlineVariable";
+import ChangeFunctionDeclaration from "./class2/ChangeFunctionDeclaration";
 
 function ReconsitutionRouter(props) {
     return <div className='x_recons pad'>
         <Switch>
-            <Route path='/recons/class1' component={Class1Router}/>
-            <Route path='/recons/class2' component={Class2Router}/>
+            <Route path='/recons/class1/part1' component={Part1}/>
+
+            <Route path='/recons/class2/extractFunction' component={ExtractFunction}/>
+            <Route path='/recons/class2/inlineFunction' component={InlineFunction}/>
+            <Route path='/recons/class2/extractVariable' component={ExtractVariable}/>
+            <Route path='/recons/class2/inlineVariable' component={InlineVariable}/>
+            <Route path='/recons/class2/changeFunctionDeclaration' component={ChangeFunctionDeclaration}/>
+
             <Route component={ReconsitutionHome}/>
         </Switch>
     </div>
