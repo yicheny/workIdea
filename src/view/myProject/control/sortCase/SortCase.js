643538bdeb399f509484491b95f429386b63b6f9
yicheny <1463020554@qq.com>
2019-10-24 16:03:13 +0800

    update IconDemo添加动画效果

--- src/view/myProject/control/sortCase/SortCase.js
+++ src/view/myProject/control/sortCase/SortCase.js
@@ -1,51 +1,62 @@
-import React,{useState,useRef} from 'react';
+import React, {useState, useRef, useEffect} from 'react';
 import {Button, Container, Progress} from "../../../../component";
-import {genListCyclic} from "../../../../utils/publicFun";
-
-const colorList = ['red','orange','yellow','green','paleturquoise','blue','purple'];
-const initNumList = [1,3,8,6,4,3,5];
+import {genListCyclic, sleep,pick} from "../../../../utils/publicFun";
+import _ from 'lodash';
 
 function SortCase(props) {
-    const colorCyclicFn = genListCyclic(colorList);
-    const [list,setList] = useState(initNumList) ;
-    // const [sortFn,setSortFn] = useState(()=>{});
-    const sortFn = useRef(()=>{});
-    const [flag,setFlag] = useState(false);
-    const max = Math.max(...initNumList)/100;
-
-    // flag && sortFn.current();
+    const [nums,setNums] = useState(new NumObjList().numObjs());
+    const iterator = useRef([].entries());
+    const {done,value} = iterator.current.next();
+    !done && setNums(new NumObjList(value.map(item=>item.value)).numObjs());
     return <Container header='排序演示'>
-        <Button onClick={()=>handleClick(bubblingSort)}>点击排序</Button>
+        <Button onClick={()=>handleClick(BubblingSort)}>点击排序</Button>
         <div className="mar_wrap_b">
             {
-                list.map((el,i)=>{
+                nums.map((el,i)=>{
                     return <div key={i} className='flex'>
-                        <Progress percent={el/max} wrapHeight={18} strokeColor={colorCyclicFn(i)}/>
-                        {/*<span style={{marginLeft:12}}>第{i+1}个数</span>*/}
+                        <Progress percent={el.percent} wrapHeight={18} strokeColor={el.color}/>
+                        <span style={{marginLeft:12}}>第{el.index}个</span>
                     </div>
                 })
             }
         </div>
     </Container>;
     function handleClick(arithmetic) {
-        arithmetic();
-        // setFlag(true);
-        // sortFn.current = arithmetic;
-    }
-
-    function bubblingSort(){
-        let len = list.length;
-        while(len){
-            for(let i=0;i<list.length-1;i++){
-                if(list[i]>list[i+1]){
-                    [list[i],list[i+1]] = [list[i+1],list[i]]
-                }
-            }
-            len--;
-        }
-        setList([...list]);
-        // setFlag(false);
+        iterator.current = BubblingSort(nums);
+        setNums([...nums])
     }
 }
 
+function* BubblingSort(list){
+    let len = list.length;
+    while(len){
+        for(let i=0;i<list.length-1;i++){
+            if(list[i]>list[i+1]){
+                [list[i],list[i+1]] = [list[i+1],list[i]]
+            }
+        }
+        len--;
+        yield list;
+    }
+}
+
+class NumObjList {
+    constructor(nums=[9,8,2,6,4,3,5,1,7]){
+        this.nums = nums;
+    }
+
+    numObjs = ()=>{
+        const colorList = ['red','orange','yellow','green','paleturquoise','blue','purple'];
+        const colorCyclic = genListCyclic(colorList);
+        const max = Math.max(...this.nums);
+
+        return this.nums.map((el,i)=>({
+            index:i+1,
+            value:el,
+            color:colorCyclic(),
+            percent:(el/max)*100
+        }))
+    };
+
+}
 export default SortCase;
