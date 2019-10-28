50b8a84a299ae7d03f0871f7f19626ea5b92d87d
yicheny <1463020554@qq.com>
2019-10-28 14:03:46 +0800

    update 计算类测试实现

--- src/base/Operation.js
+++ src/base/Operation.js
@@ -9,18 +9,18 @@ export default class Operation {
 
     resultFor = ()=>{
         if(!this.check()) return ;
-        console.log(this.algorithm[this.operate]);
+        return this.algorithm[this.operate]();
     };
 
     algorithm = {
-        '+': this.num1 + this.num2,
-        '-': this.num1 - this.num2,
-        '*': this.num1 * this.num2,
-        '/': this.num1 / this.num2,
+        '+': ()=>this.num1 + this.num2,
+        '-': ()=>this.num1 - this.num2,
+        '*': ()=>this.num1 * this.num2,
+        '/': ()=>this.num1 / this.num2,
     };
 
     check = ()=>{
-        if (Object.keys(this.algorithm).includes(this.operate)) return console.error('未定义的操作符');
+        if (!Object.keys(this.algorithm).includes(this.operate)) return console.error('未定义的操作符');
         if (!isNumber(this.num1)) return console.error('num1异常');
         if (!isNumber(this.num2)) return console.error('num2异常');
         return true;
--- src/view/myProject/control/OperationDemo/OperationDemo.js
+++ src/view/myProject/control/OperationDemo/OperationDemo.js
@@ -1,10 +1,29 @@
-import React from 'react';
-import {Container} from "../../../../component";
+import React, {useState} from 'react';
+import {Button, Container, TextInput} from "../../../../component";
+import Operation from "../../../../base/Operation";
 
 function OperationDemo(props) {
-    return <Container header='计算类测试'>
+    const operation = new Operation();
+    const [num1, setNum1] = useState(null);
+    const [num2, setNum2] = useState(null);
+    const [operate, setOperate] = useState(null);
+    const [res,setRes] = useState('');
 
-    </Container>
+    return <Container header='计算类测试'>
+        <div className="mar_wrap_b">
+            <p>输入数字1：<TextInput onChange={setNum1}/></p>
+            <p>输入数字2：<TextInput onChange={setNum2}/></p>
+            <p>输入运算符：<TextInput onChange={setOperate}/></p>
+            <p style={{lineHeight:'32px'}}><Button onClick={resFor}>点击获取结果</Button> 运算结果：{res}</p>
+        </div>
+    </Container>;
+
+    function resFor() {
+        operation.num1 = Number(num1);
+        operation.num2 = Number(num2);
+        operation.operate = operate;
+        setRes(operation.resultFor())
+    }
 }
 
 export default OperationDemo;
