390b8deada0393da8c91068eb1a81bc5dcf0f0ab
yicheny <1463020554@qq.com>
2019-10-28 16:48:12 +0800

    update input样式提取到公用样式index.less中

--- src/component/DropdownW/DropdownW.js
+++ src/component/DropdownW/DropdownW.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import './DropdownW.less';
+import {Icon} from "../index";
+
 
 function DropdownW(props) {
-    return (
-        <div>下拉框</div>
-    );
+    return <div className='DropdownW'>
+        <div className="input">
+            <div className="search">
+                {/*<div></div>*/}
+                <input type="text"/>
+            </div>
+            <div className="status">
+                <Icon type='arrowDown'/>
+            </div>
+        </div>
+        <div className="popup">
+
+        </div>
+    </div>
 }
 
 export default DropdownW;
