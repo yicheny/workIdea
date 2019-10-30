d797645afafc1339db0aecadfc99c5fce907a696
yicheny <1463020554@qq.com>
2019-10-30 13:31:42 +0800

    update 切换灯光实现

--- src/view/designPattern/control/state/SwitchLight.js
+++ src/view/designPattern/control/state/SwitchLight.js
@@ -1,15 +1,22 @@
-import React,{useState} from 'react';
+import React,{useState,useRef} from 'react';
 import {Container,Button} from "../../../../component";
-
-
+import Light from './Light';
 
 function SwitchLight(props) {
-    const [light,setLight] = useState('关灯');
+    const lightRef = useRef(new Light());
+    const [lightState,setLightState] = useState('关灯');
+
     return <Container header='切换灯光'>
         <div style={{lineHeight:'32px'}}>
-            <Button>切换灯光</Button> 灯光状态：{light}
+            <Button onClick={handleClick}>切换灯光</Button> 灯光状态：{lightState}
         </div>
-    </Container>
+    </Container>;
+
+    function handleClick(){
+        const light = lightRef.current;
+        light.currState.switchState(light);
+        setLightState(light.stateValue);
+    }
 }
 
 export default SwitchLight;
