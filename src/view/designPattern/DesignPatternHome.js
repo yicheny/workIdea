4cfd1633d6955d04f81babd35a117ba7e262b405
yicheny <1463020554@qq.com>
2019-10-31 09:29:41 +0800

    update 设计模式第一部分改名

--- src/view/designPattern/DesignPatternHome.js
+++ src/view/designPattern/DesignPatternHome.js
@@ -3,17 +3,17 @@ import {Link} from "react-router-dom";
 import {BoxM} from "../../component";
 
 function DesignPatternHome(props) {
-    return <BoxM tit='设计模式'>
-        <Link to='/design/base1'>基础常识01</Link>
-        <Link to='/design/prototype'>原型模式</Link>
-        <Link to='/design/singleton'>单例模式</Link>
-        <Link to='/design/strategy'>策略模式</Link>
-        <Link to='/design/proxy'>代理模式</Link>
-        <Link to='/design/iterator'>迭代器模式</Link>
-        <Link to='/design/pubSub'>发布-订阅模式</Link>
-        <Link to='/design/order'>命令模式</Link>
-        <Link to='/design/combination'>组合模式</Link>
-        <Link to='/design/state'>状态模式</Link>
+    return <BoxM tit='初步接触_练习'>
+        <Link to='/design/practice/base1'>基础常识01</Link>
+        <Link to='/design/practice/prototype'>原型模式</Link>
+        <Link to='/design/practice/singleton'>单例模式</Link>
+        <Link to='/design/practice/strategy'>策略模式</Link>
+        <Link to='/design/practice/proxy'>代理模式</Link>
+        <Link to='/design/practice/iterator'>迭代器模式</Link>
+        <Link to='/design/practice/pubSub'>发布-订阅模式</Link>
+        <Link to='/design/practice/order'>命令模式</Link>
+        <Link to='/design/practice/combination'>组合模式</Link>
+        <Link to='/design/practice/state'>状态模式</Link>
     </BoxM>
 }
 
--- src/view/designPattern/DesignPatternRouter.js
+++ src/view/designPattern/DesignPatternRouter.js
@@ -14,16 +14,16 @@ import SwitchLight from "./control/state/SwitchLight";
 
 function DesignPatternRouter(props) {
     return <Switch>
-        <Route path='/design/base1' component={Base1}/>
-        <Route path='/design/prototype' component={PrototypePattern}/>
-        <Route path='/design/singleton' component={SingleTon}/>
-        <Route path='/design/strategy' component={Strategy}/>
-        <Route path='/design/proxy' component={Proxy}/>
-        <Route path='/design/iterator' component={Iterator}/>
-        <Route path='/design/pubSub' component={PubSub}/>
-        <Route path='/design/order' component={Order}/>
-        <Route path='/design/combination' component={Combination}/>
-        <Route path='/design/state' component={SwitchLight}/>
+        <Route path='/design/practice/base1' component={Base1}/>
+        <Route path='/design/practice/prototype' component={PrototypePattern}/>
+        <Route path='/design/practice/singleton' component={SingleTon}/>
+        <Route path='/design/practice/strategy' component={Strategy}/>
+        <Route path='/design/practice/proxy' component={Proxy}/>
+        <Route path='/design/practice/iterator' component={Iterator}/>
+        <Route path='/design/practice/pubSub' component={PubSub}/>
+        <Route path='/design/practice/order' component={Order}/>
+        <Route path='/design/practice/combination' component={Combination}/>
+        <Route path='/design/practice/state' component={SwitchLight}/>
         <Route component={DesignPatternHome}/>
     </Switch>
 }
