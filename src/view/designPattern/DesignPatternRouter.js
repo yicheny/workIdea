95191ebdccbb82348643aebcae7d1e7a28ce37e6
yicheny <1463020554@qq.com>
2019-10-31 09:37:25 +0800

    update 设计模式目录下control改名practice

--- src/view/designPattern/DesignPatternRouter.js
+++ src/view/designPattern/DesignPatternRouter.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import {Route, Switch} from "react-router-dom";
 import DesignPatternHome from "./DesignPatternHome";
-import Base1 from "./control/Base1";
-import PrototypePattern from "./control/PrototypePattern";
-import SingleTon from "./control/SingleTon";
-import Strategy from "./control/Strategy";
-import Proxy from "./control/Proxy";
-import Iterator from "./control/Iterator";
-import PubSub from "./control/PubSub";
-import Order from "./control/Order";
-import Combination from "./control/Combination";
-import SwitchLight from "./control/state/SwitchLight";
+import Base1 from "./practice/Base1";
+import PrototypePattern from "./practice/PrototypePattern";
+import SingleTon from "./practice/SingleTon";
+import Strategy from "./practice/Strategy";
+import Proxy from "./practice/Proxy";
+import Iterator from "./practice/Iterator";
+import PubSub from "./practice/PubSub";
+import Order from "./practice/Order";
+import Combination from "./practice/Combination";
+import SwitchLight from "./practice/state/SwitchLight";
 
 function DesignPatternRouter(props) {
     return <Switch>
@@ -23,7 +23,9 @@ function DesignPatternRouter(props) {
         <Route path='/design/practice/pubSub' component={PubSub}/>
         <Route path='/design/practice/order' component={Order}/>
         <Route path='/design/practice/combination' component={Combination}/>
-        <Route path='/design/practice/state' component={SwitchLight}/>
+        <Route path='/design/practice/state' component={SwitchLight}/>、
+
+        {/*<Route path='/design/deep/purpose' component={}/>*/}
         <Route component={DesignPatternHome}/>
     </Switch>
 }
