0ca1efabc3bd6a2a881f20b10278204d0618ce36
yicheny <1463020554@qq.com>
2019-11-06 13:38:35 +0800

    add+update  foramt--P2方法增加，月度表现V2内容显示

--- src/utils/format.js
+++ src/utils/format.js
@@ -5,6 +5,7 @@ export function numFormat(value,format='N2') {
 
     if (format === 'N2') return customFormat(value.toFixed(2));
     if (format === 'N4') return customFormat(value.toFixed(4));
+    if (format === 'P2') return customFormat((value*100).toFixed(2).concat('%'));
     return console.error('numFormat函数参数错误');
 
     function customFormat(sNum) {
@@ -20,5 +21,9 @@ export function N2(value){
 
 export function N4(value){
     return numFormat(value,'N4')
+}
+
+export function P2(value){
+    return numFormat(value,'P2')
 }
 
--- src/view/work/MonthShowV2/MonthShowV2.js
+++ src/view/work/MonthShowV2/MonthShowV2.js
@@ -2,9 +2,8 @@ import React from "react";
 import _ from 'lodash';
 import {data} from "./Data/data"
 import './MonthShowV2.less';
+import {P2} from "../../../utils/format";
 import {Container,TableW as Table,ColumnW as Column} from "../../../component";
-
-const monthName = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
 
 function MonthTable(props) {
     const {data:{lData,mData}} = props;
@@ -22,22 +21,36 @@ function MonthTable(props) {
         <div className="month_table_M">
             <Table data={mData}>
                 <Column text='名称' bind='name' width={100}/>
-                <Column text='1月' bind='Jan' width={100}/>
-                <Column text='2月' bind='Feb' width={100}/>
-                <Column text='3月' bind='Mar' width={100}/>
-                <Column text='4月' bind='Apr' width={100}/>
-                <Column text='5月' bind='May' width={100}/>
-                <Column text='6月' bind='Jun' width={100}/>
-                <Column text='7月' bind='Jul' width={100}/>
-                <Column text='8月' bind='Aug' width={100}/>
-                <Column text='9月' bind='Sep' width={100}/>
-                <Column text='10月' bind='Oct' width={100}/>
-                <Column text='11月' bind='Nov' width={100}/>
-                <Column text='12月' bind='Dec' width={100}/>
+                <Column text='1月' bind='Jan' width={100} convert={dis}/>
+                <Column text='2月' bind='Feb' width={100} convert={dis}/>
+                <Column text='3月' bind='Mar' width={100} convert={dis}/>
+                <Column text='4月' bind='Apr' width={100} convert={dis}/>
+                <Column text='5月' bind='May' width={100} convert={dis}/>
+                <Column text='6月' bind='Jun' width={100} convert={dis}/>
+                <Column text='7月' bind='Jul' width={100} convert={dis}/>
+                <Column text='8月' bind='Aug' width={100} convert={dis}/>
+                <Column text='9月' bind='Sep' width={100} convert={dis}/>
+                <Column text='10月' bind='Oct' width={100} convert={dis}/>
+                <Column text='11月' bind='Nov' width={100} convert={dis}/>
+                <Column text='12月' bind='Dec' width={100} convert={dis}/>
                 <Column text='年度累计' bind='all' width={100}/>
             </Table>
         </div>
-    </div>
+    </div>;
+
+    function dis(v,o) {
+        return <span className={cnFor()}>
+            {P2(v)}
+        </span>;
+
+        function cnFor() {
+            if([o.max,o.min].includes(v)){
+                if(v>0) return 'up';
+                if(v<0) return 'down';
+            }
+            return ''
+        }
+    }
 }
 
 class MonthShowV2 extends React.Component{
@@ -63,8 +76,11 @@ class MonthShowV2 extends React.Component{
         };
 
         function spreadArr(list,callback=()=>{}) {
+            const monthName = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
             list.forEach((el,i)=>{
                 el.month.forEach((el2,i2)=>el[monthName[i2]] = el2);
+                el.max = _.max(el.month);
+                el.min = _.min(el.month);
                 callback(el);
             });
         }
