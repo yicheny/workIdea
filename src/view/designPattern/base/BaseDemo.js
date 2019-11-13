61b7fd1e47ed4ca7e3c4c2770eae944da5dbc8dc
yicheny <1463020554@qq.com>
2019-11-13 17:51:52 +0800

    update 原型模式.md

--- src/view/designPattern/base/BaseDemo.js
+++ src/view/designPattern/base/BaseDemo.js
@@ -1,52 +1,66 @@
 import React from 'react';
+import _ from 'lodash';
 
-class Resume {
-    constructor(name) {
-        this.name = name;
-        this.sex = null;
-        this.age = null;
-        this.timeArea = null;
-        this.company = null;
+class Answer{
+    constructor(){
+        this.name = null;
+        this.selecte1 = null;
+        this.selecte2 = null;
+        this.selecte3 = null;
     }
 
-    resume = function(name){
-        this.name = name;
+    setName =()=>console.error('子类必须重写setName方法');
+    setSelectAnswer1 =()=>console.error('子类必须重写setSelectAnswer1方法');
+    setSelectAnswer2 =()=>console.error('子类必须重写setSelectAnswer2方法');
+    setSelectAnswer3 =()=>console.error('子类必须重写setSelectAnswer3方法');
+
+    printRes = function(){
+        console.log(`姓名：${this.name}`);
+        console.log(`答案1：${this.selecte1}`);
+        console.log(`答案2：${this.selecte2}`);
+        console.log(`答案3：${this.selecte3}`);
     };
 
-    setPersonInfo = function(sex, age) {
-        this.sex = sex;
-        this.age = age;
+    clone=function(){
+        return _.clone(this)
     };
 
-    setWorkExperience = function(timeArea,company){
-        this.timeArea = timeArea;
-        this.company = company;
-    };
-
-    display = function(){
-        console.log(this.name,this.sex,this.age,this.timeArea,this.company);
-    };
-
-    clone = function(){
-        return {...this};
+    cloneDeep=function(){
+        return _.cloneDeep(this)
     }
 }
 
+//具体作业子类小明_原型_具体实现
+class MingAnswerPrototype extends Answer{
+    constructor(){
+        super();
+        this.name = '小明';
+        this.selecte1 = '明明明1';
+        this.selecte2 = '明明明2';
+        this.selecte3 = '明明明3';
+    }
+
+    setName=function(name){
+        this.name = name;
+    };
+    setSelectAnswer1 =function(answer){
+        this.selecte1 = answer;
+    };
+    setSelectAnswer2 =function(answer){
+        this.selecte2 = answer;
+    };
+    setSelectAnswer3 =function(answer){
+        this.selecte3 = answer;
+    };
+}
+
 function BaseDemo(props) {
-    const a = new Resume('大猫');
-    a.setPersonInfo('男','11');
-    a.setWorkExperience('2222','东京东京');
-
-    const b = a.clone();
-    b.resume('隆隆隆');
-    b.setWorkExperience('3333','西京洗净');
-
-    const c = a.clone();
-    c.setPersonInfo('女','99');
-
-    a.display();
-    b.display();
-    c.display();
+    const ming = new MingAnswerPrototype();
+    ming.printName = function(){
+        return console.log(`我的名字是：${this.name}`)
+    }
+    const long = ming.clone();
+    long.printName();//看！小龙继承了小明扩展的方法
     return <div></div>
 }
 
