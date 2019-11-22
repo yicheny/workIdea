2dc0b19052796e4f374879750107f67ae33637e8
yicheny <1463020554@qq.com>
2019-11-22 10:08:18 +0800

    add 引入ylf_public_fun 0.0.3版本并进行测试

--- src/Demo.js
+++ src/Demo.js
@@ -1,55 +1,55 @@
 import React from 'react';
-import ylf from 'ylf_npm_demo';
+import * as ylf from 'ylf_public_fun/publicFun';
 
 function Demo(props) {
     //中国员工抽象类【定义公共接口】
-    class CStaff{
-        '编程' = ()=>console.error('子类必须重定义 编程 方法');
-        '测试' = ()=>console.error('子类必须重定义 测试 方法');
-        '设计' = ()=>console.error('子类必须重定义 设计 方法');
+    class CStaff {
+        '编程' = () => console.error('子类必须重定义 编程 方法');
+        '测试' = () => console.error('子类必须重定义 测试 方法');
+        '设计' = () => console.error('子类必须重定义 设计 方法');
     }
 
-//中国员工具体类小明
-    class CStaffMing extends CStaff{
-        '编程' = ()=>{
+    //中国员工具体类小明
+    class CStaffMing extends CStaff {
+        '编程' = () => {
             console.log('小明开始编程啦！');
-        }
+        };
 
-        '测试' = ()=>{
+        '测试' = () => {
             console.log('小明开始测试啦！');
-        }
+        };
 
-        '设计' = ()=>{
+        '设计' = () => {
             console.log('小明开始设计啦！');
         }
     }
 
-//外国员工抽象类
-    class FStaff{
-        code = ()=>console.error('子类必须重定义code方法');
-        test = ()=>console.error('子类必须重定义test方法');
-        design = ()=>console.error('子类必须重定义design方法');
+    //外国员工抽象类
+    class FStaff {
+        code = () => console.error('子类必须重定义code方法');
+        test = () => console.error('子类必须重定义test方法');
+        design = () => console.error('子类必须重定义design方法');
     }
 
-//适配器类
-    class AdapterStaff extends FStaff{
-        constructor(){
+    //适配器类
+    class AdapterStaff extends FStaff {
+        constructor() {
             super();
             this.adaptee = new CStaffMing();//这里也可以传入
         }
 
-        code = ()=>this.adaptee.编程();
-        test = ()=>this.adaptee.测试();
-        design = ()=>this.adaptee.设计();
+        code = () => this.adaptee.编程();
+        test = () => this.adaptee.测试();
+        design = () => this.adaptee.设计();
     }
 
-//客户端【执行过程】
+    //客户端【执行过程】
     const staff = new AdapterStaff();
     staff.code();
     staff.test();
     staff.design();
 
-    ylf.printName();
+    console.log(ylf);
     return (
         <div></div>
     );
