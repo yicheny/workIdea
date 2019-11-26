8f7f5ed30ff053c4cbc7e439ec6c6290aa3d19d5
yicheny <1463020554@qq.com>
2019-11-26 17:23:51 +0800

    update 井字棋添加悔棋功能

--- src/view/reactDemo/project/Tictactoe.js
+++ src/view/reactDemo/project/Tictactoe.js
@@ -1,30 +1,38 @@
 import React, {useState,useMemo} from 'react';
 import {Button, Container} from "../../../component";
+import {last} from "../../../utils/publicFun";
 import './Tictactoe.less';
 
 function Tictactoe(props) {
     const chessMap = useMemo(()=>({
-        '小明':'X',
-        '小华':'O'
+        '玩家1':'X',
+        '玩家2':'O'
     }),[]);
     const initialValues = useMemo(()=>Array(9).fill(null),[]);
-    const initialPlayer = '小明';
+    const initialPlayer = '玩家1';
     const initialWinner = null;
+    const initialRecord = [];
 
     const [values,setValues] = useState(initialValues);
     const [currPlayer,setCurrPlayer] = useState(initialPlayer);
     const [winner,setWinner] = useState(initialWinner);
+    const [records,setRecords] = useState(initialRecord);
 
     return <Container header='井字棋'>
         <div className="tictactoe">
-            <div><Button type='primary' onClick={reset}>再来一局</Button></div>
+            <div>
+                <Button type='primary' onClick={reset}>再来一局</Button>
+                <Button type='primary' onClick={toPrev} style={{marginLeft:12}}>悔棋</Button>
+            </div>
             <div>胜利者：<span className='tictactoe_winner'>{winner}</span></div>
             <div>当前玩家：{currPlayer}</div>
             <div>当前玩家执子：{chessMap[currPlayer]}</div>
-            <div className="tictactoe_board">
-                {
-                    values.map((v,i)=><Button key={i} onClick={()=>handleClick(i)}>{v}</Button>)
-                }
+            <div className="flex">
+                <div className="tictactoe_board">
+                    {
+                        values.map((v,i)=><Button key={i} onClick={()=>handleClick(i)}>{v}</Button>)
+                    }
+                </div>
             </div>
         </div>
     </Container>;
@@ -35,13 +43,23 @@ function Tictactoe(props) {
         if(newValues[i]) return;
         newValues[i] = chessMap[currPlayer];
         setValues(newValues);
+        setRecords(records.concat([newValues]));
         if(isWinner(newValues)) return setWinner(currPlayer);
-        setCurrPlayer(curr=>curr==='小明'?'小华':'小明');
+        setCurrPlayer(curr=>curr==='玩家1'?'玩家2':'玩家1');
     }
 
     function reset() {
         setValues(initialValues);
         setCurrPlayer(initialPlayer);
+        setWinner(initialWinner);
+        setRecords(initialRecord);
+    }
+
+    function toPrev() {
+        if(!records.length) return;
+        records.pop();
+        setValues(last(records) || initialValues);
+        if(!winner) return setCurrPlayer(curr=>curr==='玩家1'?'玩家2':'玩家1');
         setWinner(initialWinner);
     }
 
