575e419825fa1a1c5df4bc05a6cb734cc407e35a
yicheny <1463020554@qq.com>
2019-11-27 14:23:34 +0800

    add 六维展示实现

--- src/view/myProject/SixDimShow/SixDimShow.js
+++ src/view/myProject/SixDimShow/SixDimShow.js
@@ -1,10 +1,82 @@
-import React from 'react';
-import {Container} from "../../../component";
+import React, {memo, useState} from 'react';
+import {Container, TextInput} from "../../../component";
+import ReactEcharts from 'echarts-for-react';
+
+const SixDimRadar = memo(function SixDimRadar(props) {
+    const {data}  = props;
+    let i = -1;
+    const option = {
+        title: {
+            text: '能力六维图'
+        },
+        radar: {
+            shape: 'circle',
+            name: {
+                rich: {
+                    a: {
+                        color: 'red',
+                        lineHeight: 20
+                    },
+                    b: {
+                        color: '#fff',
+                        align: 'center',
+                        backgroundColor: '#777',
+                        padding: [4, 6],
+                        borderRadius: 4,
+                    }
+                },
+                formatter: (v)=>{
+                    i++;
+                    return `{a|${v}}\n{b|${data[i] || 0}级}`
+                }
+            },
+            radius: 90,
+            indicator: [
+                {name: '攻击', max: 5},
+                {name: '防御', max: 5},
+                {name: '速度', max: 5},
+                {name: '持续', max: 5},
+                {name: '范围', max: 5},
+                {name: '开发度', max: 5}
+            ],
+        },
+        color: ['#F35541', '#5397FF', '#F5A623'],
+        series: [{
+            type: 'radar',
+            areaStyle: {
+                // normal: {},
+                opacity: 0.60
+            },
+            data: [
+                {
+                    value: data,
+                    name: '能力等级'
+                },
+            ]
+        }]
+    };
+
+    return <div style={{width:480}}>
+        <ReactEcharts option={option}/>
+    </div>
+});
+SixDimRadar.defaultProps = {
+    data:[1,1,1,0,0,0]
+};
 
 function SixDimShow(props) {
+    const [data,setData] = useState([5, 3, 5, 1, 1, 4]);
     return <Container header='六维展示'>
+        <div style={{margin:8}}>
+            <span>请输入六维值：</span>
+            <TextInput onChange={handleChange} max={555555}/>
+        </div>
+        <SixDimRadar data={data}/>
+    </Container>;
 
-    </Container>
+    function handleChange(v) {
+        setData(v.split(''));
+    }
 }
 
 export default SixDimShow;
