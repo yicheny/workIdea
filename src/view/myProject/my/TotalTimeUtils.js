85806b1d4386798e50c849c13aa52113325368f7
yicheny <1463020554@qq.com>
2019-11-27 11:47:13 +0800

    add Timebar添加持续计时与重置功能

--- src/view/myProject/my/TotalTimeUtils.js
+++ src/view/myProject/my/TotalTimeUtils.js
@@ -1,21 +1,23 @@
 import React, {useEffect, useState,useRef} from 'react';
-import {Container,Icon} from "../../../component";
+import {Button, Container, Icon} from "../../../component";
 import {dateSymFor} from "../../../utils/date";
 
 const TimeBar = React.memo(function() {
     const [status,setStatus] = useState('stop');
     const [startTime,setStartTime] = useState(null);
     const [time,setTime] = useState(0);
+    const [saveTime,setSaveTime] = useState(0);
     const timeId = useRef(null);
 
     useEffect(()=>{
         if(status==='start'){
             timeId.current = setTimeout(()=>{
-                setTime((new Date()).getTime() - startTime)
+                const leadTime = (new Date()).getTime() - startTime;
+                setTime(leadTime + saveTime);
             },1000)
         }
         return ()=>clearTimeout(timeId.current)
-    },[status,time]);
+    },[status,time,saveTime]);
 
     return <div className='flex center-y'>
         <Icon type={status==='start'?'stop':'start'} color='#1890ff' size={32}
@@ -23,15 +25,24 @@ const TimeBar = React.memo(function() {
               style={{'cursor':'pointer'}}
         />
         <span>{timeDis()}</span>
+        <Button style={{marginLeft:12}} onClick={reTime}>重置</Button>
     </div>;
 
     function handleClick() {
-        if(status==='stop'){
-            if(!startTime) setStartTime((new Date()).getTime());
+        if(status==='stop'){//点击开始
+            setStartTime((new Date()).getTime());
             return setStatus('start');
         }
-        setStartTime(0);
+
+        //点击停止
+        setSaveTime(time);
         return setStatus('stop');
+    }
+
+    function reTime() {
+        setStartTime((new Date()).getTime());
+        setSaveTime(0);
+        setTime(0);
     }
 
     function timeDis() {
@@ -51,7 +62,21 @@ function TotalTimeUtils(props) {
     return <Container header='计时工具'>
         <TimeBar/>
         <TimeBar/>
+        <TimeBar/>
+        <TimeBar/>
+        <TimeBar/>
+        <TimeBar/>
     </Container>;
 }
 
 export default TotalTimeUtils;
+
+//逻辑流程
+//开始
+//1. 点击开始，记录startTime【时间戳】，修改计时状态为`start`
+//2. 判断，如果此时计时状态为`start`，则开启计时器，计时器每隔1000ms执行一次回调
+//2.1 回调计算当前时间与startDate时间差【leadTime】，将leadTime与saveTime之和存入time状态变量
+//3. time格式化后显示
+
+//停止
+//1. 点击暂停，saveTime记录当前time状态变量，time清零，修改状态为`stop`
