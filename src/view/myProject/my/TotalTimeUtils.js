da17a1bfccadd11077ab3ba583aa2bf86218f036
yicheny <1463020554@qq.com>
2019-11-27 11:15:56 +0800

    update 将计时工具提取成TimeBar组件

--- src/view/myProject/my/TotalTimeUtils.js
+++ src/view/myProject/my/TotalTimeUtils.js
@@ -1,38 +1,36 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState,useRef} from 'react';
 import {Container,Icon} from "../../../component";
 import {dateSymFor} from "../../../utils/date";
 
-let timeId;
-function TotalTimeUtils(props) {
+const TimeBar = React.memo(function() {
     const [status,setStatus] = useState('stop');
     const [startTime,setStartTime] = useState(null);
     const [time,setTime] = useState(0);
+    const timeId = useRef(null);
 
     useEffect(()=>{
         if(status==='start'){
-             timeId = setTimeout(()=>{
-                 setTime((new Date()).getTime() - startTime)
-             },1000)
+            timeId.current = setTimeout(()=>{
+                setTime((new Date()).getTime() - startTime)
+            },1000)
         }
-        return ()=>clearTimeout(timeId)
+        return ()=>clearTimeout(timeId.current)
     },[status,time]);
 
-    return <Container header='计时工具'>
-        <div className='flex center-y'>
-            <Icon type={status==='start'?'stop':'start'} color='#1890ff' size={32}
-                  onClick={handleClick}
-                  style={{'cursor':'pointer'}}
-            />
-            <span>{timeDis()}</span>
-        </div>
-    </Container>;
+    return <div className='flex center-y'>
+        <Icon type={status==='start'?'stop':'start'} color='#1890ff' size={32}
+              onClick={handleClick}
+              style={{'cursor':'pointer'}}
+        />
+        <span>{timeDis()}</span>
+    </div>;
 
     function handleClick() {
         if(status==='stop'){
-            setStartTime((new Date()).getTime());
+            if(!startTime) setStartTime((new Date()).getTime());
             return setStatus('start');
         }
-        setTime(0);
+        setStartTime(0);
         return setStatus('stop');
     }
 
@@ -47,6 +45,13 @@ function TotalTimeUtils(props) {
             return {hour, min, sec};
         }
     }
+});
+
+function TotalTimeUtils(props) {
+    return <Container header='计时工具'>
+        <TimeBar/>
+        <TimeBar/>
+    </Container>;
 }
 
 export default TotalTimeUtils;
