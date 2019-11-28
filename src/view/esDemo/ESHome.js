82cf7849f3736419e373338ca87e68ca930a47ce
yicheny <1463020554@qq.com>
2019-11-28 09:35:30 +0800

    del 删除ts目录

--- src/view/esDemo/ESHome.js
+++ src/view/esDemo/ESHome.js
@@ -4,7 +4,6 @@ import {BoxM} from "../../component";
 
 function ESHome(props) {
     return <BoxM tit='原生知识'>
-        <Link to='/es/ts1'>TS1</Link>
         <Link to='/es/indexedDB'>indexedDB</Link>
         <Link to='/es/deepClone'>深拷贝</Link>
         <Link to='/es/reg'>正则表达式</Link>
--- src/view/esDemo/ESRouter.js
+++ src/view/esDemo/ESRouter.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import {Switch,Route} from 'react-router-dom';
+import Md from "../common/MdContaier";
+
 import ESHome from "./ESHome";
-import TS1 from "./TS/TS1.js";
 import IndexedDbDemo from "./IndexedDB/IndexedDBDemo";
 import DeepClonePath from './doc/深拷贝.md';
 import RegPath from './doc/正则表达式应用.md';
-import Md from "../common/MdContaier";
 
 function ESRouter() {
     return <div>
         <Switch>
-            <Route path='/es/ts1' component={TS1}/>
             <Route path='/es/indexedDB' component={IndexedDbDemo}/>
             <Route path='/es/deepClone' component={Md(DeepClonePath)}/>
             <Route path='/es/reg' component={Md(RegPath)}/>}/>
