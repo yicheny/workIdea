e290097eacb44c6dd9b9923ee2a61442eb8e1730
yicheny <1463020554@qq.com>
2019-11-28 11:47:57 +0800

    update MdHtml字体大小及颜色

--- src/Demo.js
+++ src/Demo.js
@@ -1,57 +1,25 @@
 import React from 'react';
-import * as ylf from 'ylf_public_fun/publicFun';
+// import * as ylf from 'ylf_public_fun/publicFun';
+import axios from 'axios';
 
 function Demo(props) {
-    //中国员工抽象类【定义公共接口】
-    class CStaff {
-        '编程' = () => console.error('子类必须重定义 编程 方法');
-        '测试' = () => console.error('子类必须重定义 测试 方法');
-        '设计' = () => console.error('子类必须重定义 设计 方法');
+    // console.log(ylf);
+    async function request() {
+        const data = await api();
+        console.log('data',data);
     }
 
-    //中国员工具体类小明
-    class CStaffMing extends CStaff {
-        '编程' = () => {
-            console.log('小明开始编程啦！');
-        };
-
-        '测试' = () => {
-            console.log('小明开始测试啦！');
-        };
-
-        '设计' = () => {
-            console.log('小明开始设计啦！');
-        }
+    function api() {
+        const url = 'https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Math/abs';
+        return axios.get(url)
     }
 
-    //外国员工抽象类
-    class FStaff {
-        code = () => console.error('子类必须重定义code方法');
-        test = () => console.error('子类必须重定义test方法');
-        design = () => console.error('子类必须重定义design方法');
-    }
+    request();
+    console.log(18);
 
-    //适配器类
-    class AdapterStaff extends FStaff {
-        constructor() {
-            super();
-            this.adaptee = new CStaffMing();//这里也可以传入
-        }
-
-        code = () => this.adaptee.编程();
-        test = () => this.adaptee.测试();
-        design = () => this.adaptee.设计();
-    }
-
-    //客户端【执行过程】
-    const staff = new AdapterStaff();
-    staff.code();
-    staff.test();
-    staff.design();
-
-    console.log(ylf);
     return (
-        <div></div>
+        <div>
+        </div>
     );
 }
 
--- src/setting.js
+++ src/setting.js
@@ -1,6 +1,6 @@
 const setting = {
     theme: 'work',//可选项：my、work
-    shotUrl: `http://localhost:3020/#/react/base/demo/useImperativeHandle`,//热键D+J即可快速打开的新页面
+    shotUrl: `http://localhost:3020/#/demo`,//热键D+J即可快速打开的新页面
 };
 
 export default setting;
