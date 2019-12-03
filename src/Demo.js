f4e838535495459995b8ffe316888de1998bf287
yicheny <1463020554@qq.com>
2019-12-03 17:04:45 +0800

    U 执行机制.md

--- src/Demo.js
+++ src/Demo.js
@@ -1,29 +1,34 @@
 import React from 'react';
-import {Button} from "./component";
 // import * as ylf from 'ylf_public_fun/publicFun';
+import axios from 'axios';
 
-function Child(props) {
-    return <div>
-        <Button type='primary' onClick={props.onClick}>Child</Button>
-    </div>
+let res = [];
+
+function complexOpe(data) { //复杂运算
+    return data.map((el,i)=>el);
 }
 
-function Control(props) {
-    const {Opt} = props;
-
-    return <div>
-        {Opt}
-    </div>
+function responseV2(data) {
+    const chunk = data.splice(0,1000);
+    res = res.concat(complexOpe(chunk));
+    if(data.length>0){
+        setTimeout(function () {
+            responseV2(data);
+        },0);
+        return null;
+    }
+    console.log(res);
 }
 
 function Demo(props) {
-    
 
+    axios.get('').then(res=>{
+        const mockData = Array.from(Array(30000),(item,i)=>i);
+        responseV2(mockData)
+    });
     return <div>
-        {Control({
-            Opt:<Child onClick={()=>console.log('click')}/>
-        })}
-        </div>;
+        <button onClick={()=>console.log("点击成功")}>点击</button>
+    </div>;
 }
 
 export default Demo;
