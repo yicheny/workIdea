fe71e7ad86dbb2a003d09e6ea00729516206ccd9
yicheny <1463020554@qq.com>
2019-12-04 17:57:11 +0800

    U 回调

--- src/Demo.js
+++ src/Demo.js
@@ -3,35 +3,30 @@ import React from 'react';
 import axios from 'axios';
 
 function Demo(props) {
-    console.log('A');
+    function print(str,callback) {
+        console.log(str);
+        setTimeout(()=>{
+             callback && callback()
+        },1000);
+    }
 
-    setTimeout(()=>{
-        console.log('B')
-    },0);
+    function printA() {
+        print('A',printB);
+    }
+    function printB() {
+        print('B',printC);
+    }
+    function printC() {
+        print('C',printD);
+        print('E')
+    }
+    function printD() {
+        print('D')
+    }
 
-    const promise = new Promise((resolve,reject)=>{
-        try {
-            resolve()
-        }catch (e) {
-            reject()
-        }
-    });
+    printA();
+    print('F');
 
-    promise.then(()=>{
-        console.log('C')
-    }).then(()=>{
-        console.log('D')
-    }).then(()=>{
-        console.log('G')
-    });
-
-    promise.then(()=>{
-        console.log('E')
-    }).then(()=>{
-        console.log('F')
-    }).then(()=>{
-        console.log('H')
-    });
     return <div>
 
     </div>;
