fa340256c4e128e10a31b5aeb49ca3bb0a09c73d
yicheny <1463020554@qq.com>
2019-12-04 09:49:58 +0800

    U 执行机制.md

--- src/Demo.js
+++ src/Demo.js
@@ -2,32 +2,38 @@ import React from 'react';
 // import * as ylf from 'ylf_public_fun/publicFun';
 import axios from 'axios';
 
-let res = [];
-
-function complexOpe(data) { //复杂运算
-    return data.map((el,i)=>el);
-}
-
-function responseV2(data) {
-    const chunk = data.splice(0,1000);
-    res = res.concat(complexOpe(chunk));
-    if(data.length>0){
-        setTimeout(function () {
-            responseV2(data);
-        },0);
-        return null;
-    }
-    console.log(res);
-}
-
 function Demo(props) {
+    console.log('A');
 
-    axios.get('').then(res=>{
-        const mockData = Array.from(Array(30000),(item,i)=>i);
-        responseV2(mockData)
+    setTimeout(()=>{
+        console.log('B')
+    },0);
+
+    const promise = new Promise((resolve,reject)=>{
+        try {
+            resolve()
+        }catch (e) {
+            reject()
+        }
+    });
+
+    promise.then(()=>{
+        console.log('C')
+    }).then(()=>{
+        console.log('D')
+    }).then(()=>{
+        console.log('G')
+    });
+
+    promise.then(()=>{
+        console.log('E')
+    }).then(()=>{
+        console.log('F')
+    }).then(()=>{
+        console.log('H')
     });
     return <div>
-        <button onClick={()=>console.log("点击成功")}>点击</button>
+
     </div>;
 }
 
