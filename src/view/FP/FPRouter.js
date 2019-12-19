c62bbd18cb18573e16cd9ee60a542e286f5eb46b
yicheny <1463020554@qq.com>
2019-12-19 17:33:03 +0800

    A 添加函数式编程base1页面

--- src/view/FP/FPRouter.js
+++ src/view/FP/FPRouter.js
@@ -1,9 +1,13 @@
 import React,{Fragment} from 'react';
 import {Route, Switch,Link} from "react-router-dom";
 import {BoxM} from "../../component";
+import Md from "../common/MdContaier";
+
+import Base1Path from './base/doc/函数式编程Base1.md';
 
 function FpRouter(props) {
     return <Switch>
+        <Route path='/fp/base1' component={Md(Base1Path)}/>
         <Route component={FpHome}/>
     </Switch>
 }
